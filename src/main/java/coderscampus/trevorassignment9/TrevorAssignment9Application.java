5afc1d05beedebc8403308de4b00139ca5854b86
Zeky84 <97308027+Zeky84@users.noreply.github.com>
2023-04-30 17:33:16 -0400

    Initial commit

---
+++ src/main/java/coderscampus/trevorassignment9/TrevorAssignment9Application.java
@@ -0,0 +1,13 @@
+package coderscampus.trevorassignment9;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrevorAssignment9Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrevorAssignment9Application.class, args);
+	}
+
+}
---
+++ src/test/java/coderscampus/trevorassignment9/TrevorAssignment9ApplicationTests.java
@@ -0,0 +1,13 @@
+package coderscampus.trevorassignment9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrevorAssignment9ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
